4a30d9e5e368a5a53808963710daf09c421cf815
monstersprites <49540368+monstersprites@users.noreply.github.com>
2021-04-02 02:32:06 -0400

    Radzen package

--- Tarea9/obj/Debug/netcoreapp3.1/RazorDeclaration/App.razor.g.cs
+++ Tarea9/obj/Debug/netcoreapp3.1/RazorDeclaration/App.razor.g.cs
@@ -76,6 +76,20 @@ using Tarea9.Shared;
 #line default
 #line hidden
 #nullable disable
+#nullable restore
+#line 10 "C:\Users\Daniel Lora\Desktop\Blazor's Apps\Tarea9\_Imports.razor"
+using Radzen;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 11 "C:\Users\Daniel Lora\Desktop\Blazor's Apps\Tarea9\_Imports.razor"
+using Radzen.Blazor;
+
+#line default
+#line hidden
+#nullable disable
     public partial class App : Microsoft.AspNetCore.Components.ComponentBase
     {
         #pragma warning disable 1998
--- Tarea9/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/Index.razor.g.cs
+++ Tarea9/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/Index.razor.g.cs
@@ -76,6 +76,20 @@ using Tarea9.Shared;
 #line default
 #line hidden
 #nullable disable
+#nullable restore
+#line 10 "C:\Users\Daniel Lora\Desktop\Blazor's Apps\Tarea9\_Imports.razor"
+using Radzen;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 11 "C:\Users\Daniel Lora\Desktop\Blazor's Apps\Tarea9\_Imports.razor"
+using Radzen.Blazor;
+
+#line default
+#line hidden
+#nullable disable
     [Microsoft.AspNetCore.Components.RouteAttribute("/")]
     public partial class Index : Microsoft.AspNetCore.Components.ComponentBase
     {
